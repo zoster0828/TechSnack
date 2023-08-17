93af1e42c1e925db46457226d5b3dbed3cfb9cbb
zoster0828 <zoster0828@gmail.com>
2023-08-17 23:33:22 +0900

    :sparkles: leetcode 542 : bruteforce2 failed

--- LeetCode/src/main/java/codingtest/leetcode/Solution542.java
+++ LeetCode/src/main/java/codingtest/leetcode/Solution542.java
@@ -4,38 +4,50 @@ public class Solution542 {
     public int[][] updateMatrix(int[][] mat) {
         int column = mat.length;
         int row = mat[0].length;
-        int result[][] = new int[column][row];
         for(int y = 0 ; y < column ; y++) {
             for(int x = 0 ; x < row ; x++) {
-                int value = getNear(result, mat, x, y);
-                result[y][x] = value;
+                if(mat[y][x] != 0 && getNear0(mat, x, y)) {
+                    continue;
+                }
+
+                mat[y][x] = getNear(mat, x, y);
             }
         }
 
-        return result;
+        return mat;
     }
-
-    public int getNear(int result[][], int[][] mat, int x, int y) {
+    public int getNear(int[][] mat, int x, int y) {
         if(mat[y][x] == 0) {
             return 0;
         }
 
         int value = Integer.MAX_VALUE;
         if(x-1 >= 0) {
-            value = Math.min(result[y][x-1]+1, value);
+            value = Math.min(mat[y][x-1]+1, value);
         }
 
         if(y-1 >= 0) {
-            value = Math.min(result[y-1][x]+1, value);
-        }
-
-        if(x+1 < mat[0].length) {
-            value = Math.min(result[y][x+1]+1, value);
-        }
-        if(y+1 < mat.length) {
-            value = Math.min(result[y+1][x]+1, value);
+            value = Math.min(mat[y-1][x]+1, value);
         }
 
         return value;
+    }
+    public boolean getNear0(int[][] mat, int x, int y) {
+        if(x-1 >= 0) {
+            if(mat[y][x-1] == 0) return true;
+        }
+
+        if(y-1 >= 0) {
+            if(mat[y-1][x] == 0) return true;
+        }
+
+        if(x+1 < mat[0].length) {
+            if(mat[y][x+1] == 0) return true;
+        }
+        if(y+1 < mat.length) {
+            if(mat[y+1][x] == 0) return true;
+        }
+
+        return false;
     }
 }
