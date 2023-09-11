07360c619c8f463ee84b6a5bd9e537b75d929a1c
zoster0828 <zoster0828@gmail.com>
2023-09-11 22:35:08 +0900

    :sparkle: feat : Solution1282 list / map solution

---
+++ LeetCode/src/test/java/codingtest/leetcode/Solution1282Test.java
@@ -0,0 +1,17 @@
+package codingtest.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution1282Test {
+    @Test
+    void test1() {
+        Solution1282 solution1282 = new Solution1282();
+        List<List<Integer>> lists = solution1282.groupThePeople(new int[]{3, 3, 3, 3, 3, 1, 3});
+        System.out.println();
+    }
+
+}
