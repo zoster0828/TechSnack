19bda4c4e532693d6c7bf517fa4586347cc1bcd6
zoster0828 <zoster0828@gmail.com>
2023-09-22 10:42:48 +0900

    :sparkles: feat : weekly363, FAILED. 1 SOLUTION

--- LeetCode/src/main/java/codingtest/leetcode/weekly363/Solution2861.java
+++ LeetCode/src/main/java/codingtest/leetcode/weekly363/Solution2861.java
@@ -4,17 +4,22 @@ import java.util.List;
 
 public class Solution2861 {
     public int maxNumberOfAlloys(int n, int k, int budget, List<List<Integer>> composition, List<Integer> stock, List<Integer> cost) {
-        List<Integer> machine = selectMachine(composition, cost);
-        int value = 0;
-        int count = 0;
-        Integer[] stocks = stock.toArray(Integer[]::new);
-        while(value <= budget) {
-            value += generate(machine, stocks, cost);
+//        List<Integer> machine = selectMachine(composition, cost);
+        int result = Integer.MIN_VALUE;
+        for (List<Integer> machine : composition) {
+            int value = 0;
+            int count = 0;
+            Integer[] stocks = stock.toArray(Integer[]::new);
+            while(value <= budget) {
+                value += generate(machine, stocks, cost);
 
-            count++;
+                count++;
+            }
+
+            result = Math.max(result, count-1);
         }
 
-        return count-1;
+        return result;
     }
 
     private int generate(List<Integer> machine, Integer[] stocks, List<Integer> cost) {
